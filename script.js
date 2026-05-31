c120ab3ca85a62ff4bbef660a95051ff27583196
alfredputra24-gif <alfred.putra24@student.uisi.ac.id>
2026-05-31 11:51:08 +0700

    ambil elemen total biaya

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const selectKopi = document.getElementById("pilih-kopi");
     const inputJumlah = document.getElementById("jumlah-kemasan");
+    const displayTotal = document.getElementById("total-biaya");
 })
